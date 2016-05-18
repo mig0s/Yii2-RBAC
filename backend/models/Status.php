2da4e9361d1dc1847b05b927e8098c4828c35138
Mikhail Gospodarikov <mail@mig0s.tk>
2016-05-18 14:46:27 +0800

    creating RBAC models; User class changed accordingly

---
+++ backend/models/Status.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+use common\models\User;
+/**
+ * This is the model class for table "status".
+ *
+ * @property integer $id
+ * @property string $status_name
+ * @property integer $status_value
+ */
+class Status extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'status';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['status_name', 'status_value'], 'required'],
+            [['status_value'], 'integer'],
+            [['status_name'], 'string', 'max' => 45],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'status_name' => 'Status Name',
+            'status_value' => 'Status Value',
+        ];
+    }
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUsers()
+    {
+        return $this->hasMany(User::className(), ['status_id' => 'status_value']);
+    }
+}
--- common/models/User.php
+++ common/models/User.php
@@ -8,6 +8,13 @@ use yii\db\ActiveRecord;
 use yii\db\Expression;
 use yii\web\IdentityInterface;
 use yii\helpers\Security;
+use backend\models\Role;
+use yii\helpers\ArrayHelper;
+use backend\models\Status;
+use backend\models\UserType;
+use frontend\models\Profile;
+use yii\helpers\Url;
+use yii\helpers\Html;
 /**
  * User model
  *
@@ -65,6 +72,10 @@ class User extends ActiveRecord implements IdentityInterface
             ['email','required'],
             ['email','email'],
             ['email','unique'],
+            [['role_id'],'in', 'range'=>array_keys($this->getRoleList())],
+            ['status_id', 'default', 'value' => self::STATUS_ACTIVE],
+            [['status_id'],'in', 'range'=>array_keys($this->getStatusList())],
+            [['user_type_id'],'in', 'range'=>array_keys($this->getUserTypeList())],
         ];
     }
     /* Your model attribute labels */
@@ -72,6 +83,16 @@ class User extends ActiveRecord implements IdentityInterface
     {
         return [
             /* Your other attribute labels */
+
+            'roleName' => Yii::t('app', 'Role'),
+            'statusName' => Yii::t('app', 'Status'),
+            'profileId' => Yii::t('app', 'Profile'),
+            'profileLink' => Yii::t('app', 'Profile'),
+            'userLink' => Yii::t('app', 'User'),
+            'username' => Yii::t('app', 'User'),
+            'userTypeName' => Yii::t('app', 'User Type'),
+            'userTypeId' => Yii::t('app', 'User Type'),
+            'userIdLink' => Yii::t('app', 'ID'),
         ];
     }
     /**
@@ -180,5 +201,130 @@ class User extends ActiveRecord implements IdentityInterface
     public function removePasswordResetToken()
     {
         $this->password_reset_token = null;
+    }
+    /**
+     * get role relationship
+     *
+     */
+    public function getRole()
+    {
+        return $this->hasOne(Role::className(), ['role_value' => 'role_id']);
+    }
+    /**
+     * get role name
+     *
+     */
+    public function getRoleName()
+    {
+        return $this->role ? $this->role->role_name : '- no role -';
+    }
+    /**
+     * get list of roles for dropdown
+     */
+    public static function getRoleList()
+    {
+        $droptions = Role::find()->asArray()->all();
+        return Arrayhelper::map($droptions, 'role_value', 'role_name');
+    }
+    /**
+     * get status relation
+     *
+     */
+    public function getStatus()
+    {
+        return $this->hasOne(Status::className(), ['status_value' => 'status_id']);
+    }
+    /**
+     * get status name
+     *
+     */
+    public function getStatusName()
+    {
+        return $this->status ? $this->status->status_name : '- no status -';
+    }
+    /**
+     * get list of statuses for dropdown
+     */
+    public static function getStatusList()
+    {
+        $droptions = Status::find()->asArray()->all();
+        return Arrayhelper::map($droptions, 'status_value', 'status_name');
+    }
+    /**
+     * getUserType
+     */
+    public function getUserType()
+    {
+        return $this->hasOne(UserType::className(), ['user_type_value' => 'user_type_id']);
+    }
+    /**
+     * get user type name
+     *
+     */
+    public function getUserTypeName()
+    {
+        return $this->userType ? $this->userType->user_type_name : '- no user type -';
+    }
+    /**
+     * get list of user types for dropdown
+     */
+    public static function getUserTypeList()
+    {
+        $droptions = UserType::find()->asArray()->all();
+        return Arrayhelper::map($droptions, 'user_type_value', 'user_type_name');
+    }
+    /**
+     * get user type id
+     *
+     */
+    public function getUserTypeId()
+    {
+        return $this->userType ? $this->userType->id : 'none';
+    }
+    /**
+     * @getProfile
+     *
+     */
+    public function getProfile()
+    {
+        return $this->hasOne(Profile::className(), ['user_id' => 'id']);
+    }
+    /**
+     * @getProfileId
+     *
+     */
+    public function getProfileId()
+    {
+        return $this->profile ? $this->profile->id : 'none';
+    }
+    /**
+     * @getProfileLink
+     *
+     */
+    public function getProfileLink()
+    {
+        $url = Url::to(['profile/view', 'id'=>$this->profileId]);
+        $options = [];
+        return Html::a($this->profile ? 'profile' : 'none', $url, $options);
+    }
+    /**
+     * get user id Link
+     *
+     */
+    public function getUserIdLink()
+    {
+        $url = Url::to(['user/update', 'id'=>$this->id]);
+        $options = [];
+        return Html::a($this->id, $url, $options);
+    }
+    /**
+     * @getUserLink
+     *
+     */
+    public function getUserLink()
+    {
+        $url = Url::to(['user/view', 'id'=>$this->Id]);
+        $options = [];
+        return Html::a($this->username, $url, $options);
     }
 }
